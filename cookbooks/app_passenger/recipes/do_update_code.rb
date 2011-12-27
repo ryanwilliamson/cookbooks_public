ec70ffd7e975c118189b60a9dcea443e51aa499f
NS <n.staknov@tut.by>
2011-12-27 18:27:26 +0300

    Split setup_db_connection and do_update_code

--- cookbooks/app_passenger/recipes/do_update_code.rb
+++ cookbooks/app_passenger/recipes/do_update_code.rb
@@ -108,23 +108,6 @@ log "INFO: Creating subversion config"
     end
 
 end
-#creating database template
-log "INFO: Generating database.yml"
-template "#{node[:app_passenger][:deploy_dir].chomp}/current/config/database.yml" do
-  owner node[:app_passenger][:apache][:user]
-  source "database.yml.erb"
-  action :create_if_missing
-end
-
-#setting $RAILS_ENV
-ENV['RAILS_ENV'] = node[:app_passenger][:project][:environment]
-
-#Creating bash file for manual $RAILS_ENV setup
-log "INFO: Creating bash file for manual $RAILS_ENV setup"
-template "/etc/profile.d/rails_env.sh" do
-  mode '0744'
-  source "rails_env.erb"
-end
 
 
 rs_utils_marker :end
