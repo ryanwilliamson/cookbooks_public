810536d2ed1d5b07a287877235dee140883fb086
NS <n.staknov@tut.by>
2012-01-29 18:10:07 +0300

    removed migration cmd input remove apache app port  input

--- cookbooks/app_passenger/attributes/app_passenger.rb
+++ cookbooks/app_passenger/attributes/app_passenger.rb
@@ -2,20 +2,10 @@
 # Cookbook Name:: app_passenger
 # Attributes:: app_passenger
 #
-# Copyright 2008-2011, RightScale, Inc.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright RightScale, Inc. All rights reserved.  All access and use subject to the
+# RightScale Terms of Service available at http://www.rightscale.com/terms.php and,
+# if applicable, other agreements such as a RightScale Master Subscription Agreement.
 
 
 set_unless[:app_passenger][:rails_spawn_method]="conservative"
