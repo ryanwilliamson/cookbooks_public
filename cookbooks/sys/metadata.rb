14648501589493ee34c85ba80eda9bc29416b985
Ryan Williamson <ryan.williamson@rightscale.com>
2012-03-12 11:59:04 -0700

    Bumped version number

--- cookbooks/sys/metadata.rb
+++ cookbooks/sys/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@rightscale.com"
 license          IO.read(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE')))
 description      "Installs/Configures RightScale system utilities."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.3"
+version          "0.2"
 
 depends "rs_utils"
 
